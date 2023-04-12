61a3c704170f267511eb90285cb8826bb0cf3d9f
miladesoja <lizzwurzel@gmail.com>
2023-04-12 10:16:57 -0300

    Cambio color prueba Liz

--- exploracion/exp_2/script.js
+++ exploracion/exp_2/script.js
@@ -15,10 +15,14 @@ d3.csv("../../../dataset1_residuos.csv", d3.autoType).then((data)=>{
                         },
                         y: 'prestacion',
                         sort: { y: 'x', reverse: true },
+                        fill: 'prestacion',
                     },
                 ),
             ),
         ],
+        color:{
+            legend: true,
+        },
     });
     d3.select("#chart").append(()=> chart);
 })
